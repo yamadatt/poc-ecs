f6d2b842bdd3e355c8a2164d76366606ca8e062b
yamadatt <yamadatt@gmail.com>
2023-08-03 10:30:43 +0900

    change

--- ecs.tf
+++ ecs.tf
@@ -52,7 +52,7 @@ resource "aws_ecs_service" "main" {
   task_definition = aws_ecs_task_definition.main.arn
   launch_type     = "FARGATE"
 
-  desired_count   = 1
+  desired_count   = 4
   
 
   network_configuration {
--- src/main.go
+++ src/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(writer http.ResponseWriter, _ *http.Request) {
-	fmt.Fprint(writer, "<h1>あああああああああああああHello Worldああああああああああああああああああああ</h1>")
+	fmt.Fprint(writer, "<h1>Hello World</h1>")
 }
 
 func main() {
