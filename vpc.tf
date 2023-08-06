1ef402941b89929be1f6c9730d89306e21d6dcb8
yamadatt <yamadatt@gmail.com>
2023-08-06 21:39:35 +0900

    apply tflint

--- vpc.tf
+++ vpc.tf
@@ -59,50 +59,50 @@ resource "aws_route_table_association" "public2" {
 resource "aws_security_group" "alb_sg" {
   name        = "alb"
   description = "Allow http and https traffic."
-  vpc_id = aws_vpc.main.id
+  vpc_id      = aws_vpc.main.id
   # ここにingressを書かず、ルールはaws_security_group_ruleを使って定義する
 }
 
 # 80番ポート許可のインバウンドルール
 resource "aws_security_group_rule" "inbound_http" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
+  type      = "ingress"
+  from_port = 80
+  to_port   = 80
+  protocol  = "tcp"
   cidr_blocks = [
     "0.0.0.0/0"
   ]
 
   # ここでweb_serverセキュリティグループに紐付け
-  security_group_id = "${aws_security_group.alb_sg.id}"
+  security_group_id = aws_security_group.alb_sg.id
 }
 
 # 443番ポート許可のインバウンドルール
 resource "aws_security_group_rule" "inbound_https" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
+  type      = "ingress"
+  from_port = 443
+  to_port   = 443
+  protocol  = "tcp"
   cidr_blocks = [
     "0.0.0.0/0"
   ]
 
   # ここでweb_serverセキュリティグループに紐付け
-  security_group_id = "${aws_security_group.alb_sg.id}"
+  security_group_id = aws_security_group.alb_sg.id
 }
 
 # アウトバウンドのAll許可
 resource "aws_security_group_rule" "outbound_any" {
-  type        = "egress"
-  protocol    = -1
-  from_port   = 0
-  to_port     = 0
+  type      = "egress"
+  protocol  = -1
+  from_port = 0
+  to_port   = 0
   cidr_blocks = [
     "0.0.0.0/0"
   ]
 
   # ここでweb_serverセキュリティグループに紐付け
-  security_group_id = "${aws_security_group.alb_sg.id}"
+  security_group_id = aws_security_group.alb_sg.id
 }
 
 
@@ -115,48 +115,48 @@ resource "aws_security_group_rule" "outbound_any" {
 resource "aws_security_group" "contaner_sg" {
   name        = "contaner_sg"
   description = "Allow 8091 traffic."
-  vpc_id = aws_vpc.main.id
+  vpc_id      = aws_vpc.main.id
   # ここにingressを書かず、ルールはaws_security_group_ruleを使って定義する
 }
 
 # 80番ポート許可のインバウンドルール
 resource "aws_security_group_rule" "inbound_80" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
+  type      = "ingress"
+  from_port = 80
+  to_port   = 80
+  protocol  = "tcp"
   cidr_blocks = [
     "0.0.0.0/0"
   ]
 
   # ここでweb_serverセキュリティグループに紐付け
-  security_group_id = "${aws_security_group.contaner_sg.id}"
+  security_group_id = aws_security_group.contaner_sg.id
 }
 
 # 443番ポート許可のインバウンドルール
 resource "aws_security_group_rule" "inbound_8091" {
-  type        = "ingress"
-  from_port   = 8091
-  to_port     = 8091
-  protocol    = "tcp"
+  type      = "ingress"
+  from_port = 8091
+  to_port   = 8091
+  protocol  = "tcp"
   cidr_blocks = [
     "0.0.0.0/0"
   ]
 
   # ここでweb_serverセキュリティグループに紐付け
-  security_group_id = "${aws_security_group.contaner_sg.id}"
+  security_group_id = aws_security_group.contaner_sg.id
 }
 
 # アウトバウンドのAll許可
 resource "aws_security_group_rule" "outbound_egress" {
-  type        = "egress"
-  protocol    = -1
-  from_port   = 0
-  to_port     = 0
+  type      = "egress"
+  protocol  = -1
+  from_port = 0
+  to_port   = 0
   cidr_blocks = [
     "0.0.0.0/0"
   ]
 
   # ここでweb_serverセキュリティグループに紐付け
-  security_group_id = "${aws_security_group.contaner_sg.id}"
+  security_group_id = aws_security_group.contaner_sg.id
 }
