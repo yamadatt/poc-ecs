046f85469b0be52500fbb3b6e9e89ec154b5add5
yamadatt <yamadatt@gmail.com>
2024-03-19 22:51:27 +0900

    勉強会用にアップデート

--- efs.tf
+++ efs.tf
@@ -1,43 +1,43 @@
-## EFS
-resource "aws_security_group" "efs_sg" {
-  name   = "efs-sg"
-  vpc_id = aws_vpc.main.id
+# ## EFS
+# resource "aws_security_group" "efs_sg" {
+#   name   = "efs-sg"
+#   vpc_id = aws_vpc.main.id
 
-  ingress {
-    from_port = 2049
-    to_port   = 2049
-    protocol  = "tcp"
-    cidr_blocks = [
-      "10.0.0.0/24",
-      "133.203.185.64/32",
-      "10.0.1.0/24"
-    ]
-  }
+#   ingress {
+#     from_port = 2049
+#     to_port   = 2049
+#     protocol  = "tcp"
+#     cidr_blocks = [
+#       "10.0.0.0/24",
+#       "133.203.185.64/32",
+#       "10.0.1.0/24"
+#     ]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
 
-}
+# }
 
-resource "aws_efs_file_system" "efs" {
-  tags = {
-    Name = "test-efs"
-  }
-}
+# resource "aws_efs_file_system" "efs" {
+#   tags = {
+#     Name = "test-efs"
+#   }
+# }
 
-resource "aws_efs_mount_target" "efs_public1" {
-  file_system_id  = aws_efs_file_system.efs.id
-  subnet_id       = aws_subnet.public1.id
-  security_groups = [aws_security_group.efs_sg.id]
-}
+# resource "aws_efs_mount_target" "efs_public1" {
+#   file_system_id  = aws_efs_file_system.efs.id
+#   subnet_id       = aws_subnet.public1.id
+#   security_groups = [aws_security_group.efs_sg.id]
+# }
 
-resource "aws_efs_mount_target" "efs_public2" {
-  file_system_id  = aws_efs_file_system.efs.id
-  subnet_id       = aws_subnet.public2.id
-  security_groups = [aws_security_group.efs_sg.id]
-}
+# resource "aws_efs_mount_target" "efs_public2" {
+#   file_system_id  = aws_efs_file_system.efs.id
+#   subnet_id       = aws_subnet.public2.id
+#   security_groups = [aws_security_group.efs_sg.id]
+# }
