fede7a6d2a23760e0f8f32982807a2d5b9e9c637
yamadatt <yamadatt@gmail.com>
2024-07-06 13:13:11 +0900

    output sample log

--- src/main.go
+++ src/main.go
@@ -13,4 +13,7 @@ func handler(writer http.ResponseWriter, _ *http.Request) {
 func main() {
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8091", nil))
+	fmt.Println("info: Server worked at http://localhost:8091")
+	fmt.Println("error: Server worked at fluentbit")
+
 }
